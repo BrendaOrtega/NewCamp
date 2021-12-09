3c53517f7961f8e18b0c9c0cc05659d09a9a9ff3
hectorbliss <fixtergeek@gmail.com>
2021-12-09 00:07:38 -0600

    is better with springs

--- src/experiments/VerticalWheelCarousel.jsx
+++ src/experiments/VerticalWheelCarousel.jsx
@@ -1,55 +1,33 @@
 import * as React from 'react'
 import useDevice from '../tools/useDevice'
+import { useSpring, animated } from 'react-spring'
 
 import styles from './VerticalWheelCarousel.module.css'
 
 const VerticalWheelCarousel = (props) => {
     const { sources, flex = 1 } = props
-    const top = React.useRef(0)
-    const ref = React.useRef()
-
-    const animationFrameId = React.useRef()
-
     const device = useDevice()
-    // const device = useDevice()
-    // const style = useSpring({
-    //     from: { top: 0 },
-    //     to: { top: -1895 },
-    //     loop: true,
-    //     config: { duration: 20000 },
-    //     pause
-    // })
+    const factors = React.useRef([0, 0])
+    const [isOver, setIsOver] = React.useState(false)
 
-    let direction = 'up'
-
-    const animate = () => {
-        if (direction === 'up') {
-            top.current--
-            if (top.current < -(sources.length * 100)) {
-                direction = 'down'
+    // iteration 2
+    const initialSpringValues = ({
+        to: async next => {
+            while (1) {
+                await next({ number: -0 })
+                await next({ number: -2000 })
             }
-        } else {
-            top.current++
-            if (top.current > (sources.length * 10)) {
-                direction = 'up'
-            }
-        }
-
-        const group = document.querySelector('#first-column')
-        const group2 = document.querySelector('#second-column')
-        group.style.top = `${top.current}px`
-        group2.style.top = `${top.current}px`
-        // animationFrameId.current = window.requestAnimationFrame(animate)
-        animationFrameId.current = setTimeout(animate, 1000 / 60)
-        // return () => window.cancelAnimationFrame(animationFrameId.current)
-        return () => clearTimeout(animationFrameId.current)
-    }
-
-    React.useLayoutEffect(() => {
-        return animate()
-    }, [])
-
-    const height = (sources.length - 1) * 300
+        },
+        config: {
+            duration: 12000,
+            // tension: 250,
+            // friction: 10,
+        },
+        loop: { reverse: true },
+        from: { number: 0 },
+    })
+    const [spring, api] = useSpring(() => initialSpringValues)
+    const [{ offset }, api2] = useSpring(() => ({ offset: 0, }));
 
     const renderSources = (arr) => {
         if (typeof arr[0] === 'string') {
@@ -57,58 +35,60 @@ const VerticalWheelCarousel = (props) => {
         }
     }
 
-    const followMouse = (y) => {
-        const group = document.querySelector('#first-column')
-        const group2 = document.querySelector('#second-column')
-        const val = -y * 7
-        const currentTop = Number(group.style.top.split('px')[0])
-        // const val = y * 5
-        // const val = y
-        group.style.top = `${currentTop + val}px`
-        group2.style.top = `${currentTop + val}px`
+    const translate = (o) => `translateY(${o}px)`;
 
+    const style = ({ transform: isOver ? offset.to(translate) : spring.number.to(translate) })
+
+    const followMouse = (e) => {
+        const posY = e?.clientY
+        const offset = window.pageYOffset - posY;
+        if (isNaN(offset)) { return }
+        api2.start({
+            offset, config: {
+                tension: 170,
+                mass: 5,
+                friction: 26,
+                velocity: 0,
+                precision: 0.01
+            }
+        });
     }
 
     const onMouseOver = (event) => {
-        if (device.type === 'Mobile') { return }
-        // window.cancelAnimationFrame(animationFrameId.current)
-        clearTimeout(animationFrameId.current)
-
+        // api.pause()
+        setIsOver(true)
     }
     const onMouseOut = () => {
-        if (device.type === 'Mobile') { return }
-        animate()
+        // api.resume()
+        setIsOver(false)
     }
 
-    window.addEventListener('mousemove', e => {
-        if (device.type === 'Mobile') { return }
-        followMouse(e.clientY / window.innerHeight - 0.5)
-    })
+    React.useEffect(() => {
+        window.addEventListener("mousemove", followMouse)
+        return () => window.removeEventListener('mousemove', followMouse)
+    }, [])
 
-    // console.log(window.WURFL) <= device detection
     return (
         <div className={`${styles.container} `}
-            // style={{
-            //     flex: flex === 'xl' ? 1.5 : 1
-            // }}
-            ref={ref}
             onMouseOver={onMouseOver}
             onMouseOut={onMouseOut}
         >
             <div className={styles.overlay}></div>
-            <div
+            <animated.div
+
+                style={style}
                 id="first-column"
                 className={`${styles.group} ${styles.one}`}
             >
                 {renderSources(sources.slice(0, 3).concat(sources.slice(0, 3)))}
-            </div>
-            <div
-
+            </animated.div>
+            <animated.div
+                style={style}
                 id="second-column"
                 className={`${styles.group} ${styles.two}`}
             >
                 {renderSources(sources.slice(3).concat(sources.slice(3)))}
-            </div>
+            </animated.div>
         </div>
     )
 }
