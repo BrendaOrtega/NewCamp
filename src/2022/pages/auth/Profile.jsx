469006c89d8742f2a5ba68cd72d6a50c81981757
hectorbliss <fixtergeek@gmail.com>
2022-01-05 21:53:09 -0600

    profile wip

--- src/2022/pages/auth/Profile.jsx
+++ src/2022/pages/auth/Profile.jsx
@@ -4,6 +4,8 @@ import {
     Heading,
     Image,
     useColorMode,
+    Tabs, TabList, TabPanels, Tab, TabPanel, Box, Grid, GridItem,
+
 } from '@chakra-ui/react'
 import * as React from 'react'
 
@@ -26,41 +28,104 @@ const Profile = () => {
                 backgroundImage: `url('${cover}')`,
                 backgroundSize: 'cover',
                 position: 'relative',
-            }}>
-                <Flex
-                    flexDirection={['column', 'row']}
-                    justifyContent={['center', 'flex-start']}
-                    position="absolute"
-                    bottom={['-30%', '-20%']}
-                    right="0"
-                    left={['0', '10%']}
-                    alignItems="center"
-                    gap={[3, 5]}
-                >
-                    <Image
-                        width="170px"
-                        rounded='full'
-                        src={photoURL}
-                        alt="user pic" />
-                    <Heading
-                        pb={10}
-                        // paddingLeft="19px"
-                        color={[colorMode === 'dark' ? '#fff' : '#000', '#fff']}>
-                        {displayName}
-                    </Heading>
-                </Flex>
-            </div>
+            }}></div>
 
             <Container
                 p={0}
-                maxW="container.xl"
+                maxW="container.lg"
+                // position="relative"
+                // top="-100px"
+                marginTop="-100px"
+                zIndex="999"
+                position="relative"
             >
-                <Flex h="100vh" py={20}
+                <Flex
+                    // h="100vh" 
+
+                    py={0}
                     direction={['column', 'row']}
                 >
-                    Bliss
+                    <Grid
+
+                        minH="200px"
+                        maxH="300px"
+                        templateRows={['repeat(3, 1fr)', 'repeat(2, 1fr)']}
+                        templateColumns={['repeat(1,1fr)', 'repeat(3, 1fr)']}
+                        gap="19px"
+                        // border="2px solid red"
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <GridItem
+                            rowSpan={[1, 2]}
+                            colSpan={1}
+                            justifySelf={['center', 'left']}
+                        >
+                            <Image
+                                width="170px"
+                                rounded='full'
+                                src={photoURL}
+                                alt="user pic" />
+                        </GridItem>
+                        <GridItem
+                            colSpan={[1, 2]}
+                            justifySelf={['center', 'left']}
+                            alignSelf="flex-end"
+                        >
+                            <Heading
+                                // pb={10}
+                                // paddingLeft="19px"
+                                color={[colorMode === 'dark' ? '#fff' : '#000', '#fff']}
+                            // color="tomato"
+                            >
+                                {displayName}
+                            </Heading>
+                        </GridItem>
+                        <GridItem
+                            colSpan={[1, 2]}
+                            justifySelf={['center', 'left']}
+                            alignSelf="flex-end"
+
+                        >
+                            <Tabs
+                                // isFitted
+                                colorScheme="brand"
+                            >
+                                <TabList>
+                                    <Tab>Mi información</Tab>
+                                    <Tab>Suscripción</Tab>
+                                </TabList>
+                            </Tabs>
+                        </GridItem>
+                    </Grid>
+
                 </Flex>
             </Container>
+            <Flex
+                // position="relative"
+                // top="-100px"
+                bg={colorMode === 'dark' ? '#2E2E2E' : "#F2F2F2"}
+                w="100%"
+                // bg="#fff"
+                // paddingTop="100px"
+                justifyContent="center"
+                paddingBottom="100px"
+
+            >
+
+                <Flex
+                    borderRadius="16px"
+                    justifyContent="center"
+                    my="40px"
+                    w="1008px"
+                    h="150px"
+                    bg={colorMode === 'dark' ? '#323232' : '#fff'}
+
+                >
+                    Blissi
+                </Flex>
+            </Flex>
+
         </>
     )
 }
